ac4c5403347427e865b4113db51b0fdd114975d6
Arthur Hovhannisyan <artur_hovhannisyan@protonmail.com>
2022-06-17 12:47:31 +0400

    project init

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  bracketSameLine: true,
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#23C2A8",
+        green: "#81C719",
+        blue: "#23C2A8",
+        orange: "#FD8516",
+        red: "#F95A4E",
+        purple: "#A349A4",
+        "light-gray": "#F9F9F9",
+        "text-primary": "#3E3E3E",
+      },
+    },
+  },
+  plugins: [],
+};
